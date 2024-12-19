ca8d7b97defd0ef3da1b788f38e467d9157fffd0
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2024-12-19 12:18:31 +0700

    Initial commit

---
+++ app/src/main/java/com/nhinhnguyenuit/jetpackcomposeopencv/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.nhinhnguyenuit.jetpackcomposeopencv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nhinhnguyenuit.jetpackcomposeopencv.ui.theme.JetpackComposeOpenCVTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeOpenCVTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeOpenCVTheme {
+        Greeting("Android")
+    }
+}
